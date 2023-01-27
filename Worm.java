79e786a24036bd3648fdcb404a7f3acb27b462cf
es9dpw <es9dpw999@gmail.com>
2023-01-27 00:32:45 +0000

    commit

--- Worm.java
+++ Worm.java
@@ -4,11 +4,11 @@ public class Worm extends Actor
 {
     /**
      * The worm does nothing and is simply placed in the world for the crab to
-     * eat. All collison detection is handled in the crab so nothing is needed
+     * eat. All collision detection is handled in the crab so nothing is needed
      * here
      */
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
